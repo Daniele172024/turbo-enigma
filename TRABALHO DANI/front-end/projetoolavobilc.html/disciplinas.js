1d644f9b4dbe6e7fe15229fcc3b2ee4147d29868
Daniele172024 <161052038+Daniele172024@users.noreply.github.com>
2024-02-23 12:33:19 -0300

    Add files via upload

---
+++ TRABALHO DANI/front-end/projetoolavobilc.html/disciplinas.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $("#elemento1").click(function(){
+        $(".texto1").hide();
+        $("#conteudo1").show();
+
+    });
+    $("#elemento2").click(function(){
+        $(".texto1").hide();
+        $("#conteudo2").show();
+
+    });
+        $("#elemento3").click(function(){
+            $(".texto1").hide();
+            $("#conteudo3").show();
+    });    
+        $("#elemento4").click(function(){
+            $(".texto1").hide();
+            $("#conteudo4").show();
+        });  
+    });
